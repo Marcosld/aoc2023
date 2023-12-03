5757d9b15a90bb807dae11bb921f7143b383addd
Marcos Luis Delgado <mluis@tuenti.com>
2023-12-03 10:32:09 +0100

    Start problem 3

---
+++ 3.js
@@ -0,0 +1,18 @@
+const input = `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`;
+
+const parseInput = (input) => input.split("\n");
+
+const solve1 = (input) => {
+  const lines = parseInput(input);
+};
+
+console.log(solve1(input));
