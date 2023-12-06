f97cf27dab3ffd5c5e5a1cb3112a4a4644ec8644
Marcos Luis Delgado <mluis@tuenti.com>
2023-12-06 11:19:17 +0100

    p6 revamped :D

--- 6.mjs
+++ 6.mjs
@@ -1,42 +1,42 @@
-import { performance } from "node:perf_hooks";
-
 const input = `Time:        40     81     77     72
 Distance:   219   1012   1365   1089`;
 
 const parseInput = (input) => {
-  const timesAndDistances = input
+  const [times, distances] = input
     .split("\n")
-    .map((line) => line.split(" ").filter(Boolean).slice(1));
-  const timeDis = [];
-  for (const [i, time] of Object.entries(timesAndDistances[0])) {
-    timeDis.push([time, timesAndDistances[1][i]]);
+    .map((line) => line.split(":")[1].trim().split(/\s+/).map(Number));
+  return [times, distances];
+};
+
+const operateQuadratic = (t, d, op) =>
+  op(-t, Math.sqrt(Math.pow(t, 2) - 4 * d)) / -2;
+
+const solveQuadratic = (t, d) => [
+  operateQuadratic(t, d, (a, b) => a + b),
+  operateQuadratic(t, d, (a, b) => a - b),
+];
+
+const solve = (ts, ds) => {
+  let result = 1;
+  for (let i = 0; i < ts.length; i++) {
+    const [t, d] = [ts[i], ds[i]];
+
+    const [p1, p2] = solveQuadratic(t, d);
+    const nOfSolutions = Math.ceil(p2) - Math.floor(p1) - 1;
+    result *= nOfSolutions;
   }
-  return timeDis;
+  return result;
 };
 
 const solve1 = (input) => {
-  const timeDistances = parseInput(input);
-
-  let ans = 1;
-  for (const [time, distance] of timeDistances) {
-    let waysToWin = 0;
-    for (let holdTime = 1; holdTime < time; holdTime++) {
-      const speed = holdTime;
-
-      const runDistance = speed * (time - holdTime);
-      if (runDistance > distance) {
-        waysToWin++;
-      }
-    }
-    ans *= waysToWin;
-  }
-  return ans;
+  const [ts, ds] = parseInput(input);
+  return solve(ts, ds);
 };
 
-performance.mark("p2");
+const solve2 = (input) => {
+  const [ts, ds] = parseInput(input).map((arr) => [Number(arr.join(""))]);
+  return solve(ts, ds);
+};
+
 console.log(solve1(input));
-performance.measure("p2-measure", "p2");
-console.log(
-  "Duration (ms):",
-  performance.getEntriesByName("p2-measure")[0].duration,
-);
+console.log(solve2(input));
