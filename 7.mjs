934298e81f9ad61f9dce739559ece6b818406a3f
Marcos Luis Delgado <mluis@tuenti.com>
2023-12-07 11:10:11 +0100

    Little refacor

--- 7.mjs
+++ 7.mjs
@@ -999,7 +999,23 @@ K54KJ 164
 347J4 581
 Q7289 138`;
 
-const highValueOrder = {
+const order2 = {
+  A: 12,
+  K: 11,
+  Q: 10,
+  T: 9,
+  9: 8,
+  8: 7,
+  7: 6,
+  6: 5,
+  5: 4,
+  4: 3,
+  3: 2,
+  2: 1,
+  J: 0,
+};
+
+const order1 = {
   A: 12,
   K: 11,
   Q: 10,
@@ -1015,10 +1031,10 @@ const highValueOrder = {
   2: 0,
 };
 
-const compareByCard = (h1, h2, i = 0) =>
-  highValueOrder[h1[i]] - highValueOrder[h2[i]] || compareByCard(h1, h2, i + 1);
+const compareByCard = (h1, h2, order, i = 0) =>
+  order[h1[i]] - order[h2[i]] || compareByCard(h1, h2, order, i + 1);
 
-const solve1 = (input) => {
+const solve = (input, sortHandVals, cardOrder) => {
   const hands = [];
   for (const line of input.split("\n")) {
     const [handStr, scoreStr] = line.split(" ");
@@ -1027,21 +1043,43 @@ const solve1 = (input) => {
     for (const card of handStr) {
       handVals[card] = (handVals[card] || 0) + 1;
     }
-    const sortedHandVals = Object.values(handVals).sort((a, b) => b - a);
+    const sortedHandVals = sortHandVals(handVals);
     const handWeight = 2 * sortedHandVals[0] + (sortedHandVals[1] || 0);
 
-    hands.push([handStr, handVals, handWeight, +scoreStr]);
+    hands.push([handStr, handWeight, +scoreStr]);
   }
 
   const sortedHands = hands.sort((a, b) => {
-    const [aHandStr, , aWeight] = a;
-    const [bHandStr, , bWeight] = b;
-    return aWeight - bWeight || compareByCard(aHandStr, bHandStr);
+    const [aHandStr, aWeight] = a;
+    const [bHandStr, bWeight] = b;
+    return aWeight - bWeight || compareByCard(aHandStr, bHandStr, cardOrder);
   });
-
-  console.log(JSON.stringify(sortedHands, null, 2));
 
   return sortedHands.reduce((acc, hand, i) => acc + hand.at(-1) * (i + 1), 0);
 };
 
+const solve1 = (input) => {
+  const sortHandVals = (handVals) =>
+    Object.values(handVals).sort((a, b) => b - a);
+
+  return solve(input, sortHandVals, order1);
+};
+
+const solve2 = (input) => {
+  const sortHandVals = (handVals) => {
+    const sortedHandKeys = Object.entries(handVals)
+      .sort(([, a], [, b]) => b - a)
+      .map(([card]) => card);
+
+    if (handVals.J > 0 && handVals.J < 5) {
+      handVals[sortedHandKeys[sortedHandKeys[0] === "J" ? 1 : 0]] += handVals.J;
+      handVals.J = 0;
+    }
+    return Object.values(handVals).sort((a, b) => b - a);
+  };
+
+  return solve(input, sortHandVals, order2);
+};
+
 console.log(solve1(input));
+console.log(solve2(input));
