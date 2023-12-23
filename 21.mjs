5b85851125972122bc168ebcd4adb97f7becdb41
Marcos Luis Delgado <mluis@tuenti.com>
2023-12-23 01:15:23 +0100

    Minor refactor

--- 21.mjs
+++ 21.mjs
@@ -2,47 +2,6 @@ import { readInput, getStraightAdjacentPositions } from "./utils.mjs";
 
 const input = readInput(import.meta);
 
-const readLines = (input) => {
-  const lines = input.split("\n");
-  const [I, J] = [lines.length, lines[0].length];
-
-  return { lines, I, J };
-};
-
-const solve1 = (input) => {
-  const { lines, I, J } = readLines(input);
-
-  const rocks = new Set();
-  let positions = new Set();
-  let newPositions = new Set();
-
-  for (const [i, line] of lines.entries()) {
-    for (const [j, c] of line.split("").entries()) {
-      if (c === "#") rocks.add(JSON.stringify([i, j]));
-      if (c === "S") positions.add(JSON.stringify([i, j]));
-    }
-  }
-
-  for (let i = 0; i < 64; i++) {
-    for (const pos of positions.keys()) {
-      getStraightAdjacentPositions(...JSON.parse(pos))
-        .filter(
-          ([i, j]) =>
-            i >= 0 &&
-            j >= 0 &&
-            i < I &&
-            j < J &&
-            !rocks.has(JSON.stringify([i, j])),
-        )
-        .forEach(([i, j]) => newPositions.add(JSON.stringify([i, j])));
-    }
-    positions = newPositions;
-    newPositions = new Set();
-  }
-
-  return positions.size;
-};
-
 const isCycling = (arr, cycleLength) => {
   for (let i = 0; i < cycleLength; i++) {
     if (arr[i] !== arr[i + cycleLength]) {
@@ -59,8 +18,9 @@ const addToCircular = (arr, el, cycleLength) => {
   }
 };
 
-const solve2 = (input, targetStep) => {
-  const { lines, I, J } = readLines(input);
+const solve = (input, targetStep, hasBounds = false) => {
+  const lines = input.split("\n");
+  const [I, J] = [lines.length, lines[0].length];
 
   const rocks = new Set();
   let bounds = new Set();
@@ -81,30 +41,37 @@ const solve2 = (input, targetStep) => {
     return [nI < 0 ? I + nI : nI, nJ < 0 ? J + nJ : nJ];
   };
 
-  let result = [1n, 0n];
+  const inBounds = ([i, j]) => i >= 0 && i < I && j >= 0 && j < J;
 
   const boundPointsVariations = [];
   const diffVariations = [];
-
   const cycleLength = I;
+  let result = 0n;
   let cycleStart;
 
   let k = 0;
-  for (; k < targetStep; k++) {
+  for (; k < targetStep + 1; k++) {
     const newBounds = new Set();
     const visited = new Set();
+
+    if (k % 2 === targetStep % 2) {
+      result += BigInt(bounds.size);
+    }
+
     for (const pointStr of bounds) {
       const [oI, oJ] = JSON.parse(pointStr);
 
       visited.add(pointStr);
 
       getStraightAdjacentPositions(oI, oJ)
-        .filter(([i, j]) => !rocks.has(JSON.stringify(transpose([i, j]))))
+        .filter(
+          ([i, j]) =>
+            (!hasBounds || inBounds([i, j])) &&
+            !rocks.has(JSON.stringify(transpose([i, j]))) &&
+            !lastVisited.has(JSON.stringify([i, j])),
+        )
         .forEach(([i, j]) => {
-          const key = JSON.stringify([i, j]);
-          if (!lastVisited.has(key)) {
-            newBounds.add(key);
-          }
+          newBounds.add(JSON.stringify([i, j]));
         });
     }
 
@@ -118,7 +85,6 @@ const solve2 = (input, targetStep) => {
 
     bounds = newBounds;
     lastVisited = visited;
-    result[(k + 1) % 2] += BigInt(newBounds.size);
 
     if (isCycling(diffVariations, cycleLength)) {
       if (!cycleStart) {
@@ -139,16 +105,19 @@ const solve2 = (input, targetStep) => {
 
   k += 1; // next iteration
   const offset = k % cycleLength;
-  for (; k < targetStep; k++) {
+  for (; k < targetStep + 1; k++) {
+    if (k % 2 === targetStep % 2) {
+      result += BigInt(boundSize);
+    }
+
     const index = (k - offset) % cycleLength;
     cycleBoundPointVariations[index] += cycleDiffVariations[index];
     boundSize += cycleBoundPointVariations[index];
-    result[(k + 1) % 2] += BigInt(boundSize);
   }
 
-  return result[targetStep % 2];
+  return result;
 };
 
-console.log(solve1(input));
+console.log(solve(input, 64, true));
 console.time("run p2");
-console.timeLog("run p2", solve2(input, 26501365));
+console.timeLog("run p2", solve(input, 26501365));
